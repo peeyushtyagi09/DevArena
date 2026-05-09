4a08a879b431efaf2c80078d2a3274c7be5c785d
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2026-05-09 23:41:57 +0530

    adding my session file model

--- backend/models/Session.model.js
+++ backend/models/Session.model.js
@@ -1,14 +1,34 @@
 const mongoose = require("mongoose");
 
 const sessionSchema = new mongoose.Schema(
-    {
-        userId: {
-            type: mongoose.Schema.Objects.Types("user"),
-        }
-
-    },{
-        timestamps: true
-    }
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", 
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+      select: false, 
+    },
+    deviceInfo: {
+      type: String,
+      default: null,
+    },
+    ipAddress: {
+      type: String,
+      default: null,
+    },
+    expiredAt: {
+      type: Date, 
+      required: false,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model("session", sessionSchema);
+module.exports = mongoose.model("Session", sessionSchema);
